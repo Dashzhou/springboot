4f27f6d06062fb110c415dc839a5070ba2356da1
zyh <zyh@qq.com>
2022-03-30 19:40:43 +0800

    master 1 commit

--- src/test/java/com/zyh/springboot/test/javatest.java
+++ src/test/java/com/zyh/springboot/test/javatest.java
@@ -8,5 +8,6 @@ public class javatest {
         System.out.println("2345678");
         System.out.println("2345456789");
         System.out.println("hello,world");
+        System.out.println("hello,world master");
     }
 }
