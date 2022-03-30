7d765200f18ab5cf93a31f890514b5234ff5cefb
zyh <zyh@qq.com>
2022-03-30 19:32:28 +0800

    third commit

--- src/test/java/com/zyh/springboot/test/javatest.java
+++ src/test/java/com/zyh/springboot/test/javatest.java
@@ -6,5 +6,6 @@ package com.zyh.springboot.test;
 public class javatest {
     public static void main(String[] args) {
         System.out.println("2345678");
+        System.out.println("2345456789");
     }
 }
