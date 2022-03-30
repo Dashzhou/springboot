b2660c0bc962a958f5d5e6187be4a4e0618840de
zyh <zyh@qq.com>
2022-03-30 19:37:34 +0800

    hot-fix commit

--- src/test/java/com/zyh/springboot/test/javatest.java
+++ src/test/java/com/zyh/springboot/test/javatest.java
@@ -7,5 +7,6 @@ public class javatest {
     public static void main(String[] args) {
         System.out.println("2345678");
         System.out.println("2345456789");
+        System.out.println("hello,world");
     }
 }
